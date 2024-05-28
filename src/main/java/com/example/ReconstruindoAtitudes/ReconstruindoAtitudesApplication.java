7b5f816d002bcda3f5188794634088003c756135
Cauã Siqueira Carneiro da Cunha <105356310+CauaSiCa29@users.noreply.github.com>
2024-05-28 19:21:15 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/ReconstruindoAtitudes/ReconstruindoAtitudesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReconstruindoAtitudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconstruindoAtitudesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconstruindoAtitudesApplication.class, args);
+	}
+
+}
