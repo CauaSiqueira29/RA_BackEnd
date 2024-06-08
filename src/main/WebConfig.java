20536051543ad02a8473589705e5ef01bd149be8
Cauã Siqueira Carneiro da Cunha <105356310+CauaSiCa29@users.noreply.github.com>
2024-06-08 00:40:20 -0300

    Add link do front

--- src/main/WebConfig.java
+++ src/main/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://reconstruindoatitudes.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
